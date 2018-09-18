0fef0cdbde73e9c0b3979c6b55a638e2d09945d3
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-18 21:57:25 +0100

    Button function

--- DemoApp/app/src/main/java/com/radhikasivarajan/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/radhikasivarajan/demoapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.radhikasivarajan.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClick(View view){
+        Log.i("Infos","The button clicked");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
