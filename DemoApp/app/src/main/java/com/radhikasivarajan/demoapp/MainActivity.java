7bd75320ffec39c11a5332d0f77f98a3719c548b
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-19 17:23:38 +0100

    Simple login form

--- DemoApp/app/src/main/java/com/radhikasivarajan/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/radhikasivarajan/demoapp/MainActivity.java
@@ -4,11 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void buttonClick(View view){
-        Log.i("Infos","The button clicked");
+
+        EditText getTheName = (EditText) findViewById(R.id.getName);
+        EditText getThePassword = (EditText) findViewById(R.id.getPassword);
+
+        Log.i("User name", getTheName.getText().toString());
+        Log.i("Password", getThePassword.getText().toString());
     }
 
     @Override
