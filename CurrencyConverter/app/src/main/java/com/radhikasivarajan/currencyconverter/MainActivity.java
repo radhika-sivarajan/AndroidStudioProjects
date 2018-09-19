4da97187d4761e0884cc06253ddc73e3edaae5ca
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-19 20:31:17 +0100

    Currency converter

--- CurrencyConverter/app/src/main/java/com/radhikasivarajan/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/radhikasivarajan/currencyconverter/MainActivity.java
@@ -2,8 +2,22 @@ package com.radhikasivarajan.currencyconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertFunction(View view){
+
+        EditText getTheAmount = (EditText) findViewById(R.id.getAmount);
+        Double dollarAmountDouble = Double.parseDouble(getTheAmount.getText().toString());
+        Double poundAmount = dollarAmountDouble * 0.76;
+        Toast.makeText(this, "£" + poundAmount.toString(), Toast.LENGTH_SHORT).show();
+
+        Log.i("Test","It works");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
