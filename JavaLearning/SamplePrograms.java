784dcb69ce5b5117c283f072d6b63562573dd063
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-22 22:54:52 +0100

    Variables

--- JavaLearning/SamplePrograms.java
+++ JavaLearning/SamplePrograms.java
@@ -1,5 +1,14 @@
-public class HelloWorld{
-	public static void main(String[] args){
-		System.out.println("Hello world");
-	}
-}
+public class Variables {
+    public static void main(String[] args) {
+      int age =34;
+      double favNo = 3.3;
+      System.out.println("My favorite number is "+ favNo);
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
