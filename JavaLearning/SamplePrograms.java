ff9072f545f9a565f19e6eb7e71047f583f68343
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-25 21:37:17 +0100

    Var types

--- JavaLearning/SamplePrograms.java
+++ JavaLearning/SamplePrograms.java
@@ -2,6 +2,7 @@ public class Variables {
     public static void main(String[] args) {
       int age =34;
       double favNo = 3.3;
+      boolean loggedin = true;
       System.out.println("My favorite number is "+ favNo);
     }
   }
