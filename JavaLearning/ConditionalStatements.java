5be8d4413d446ce8d1471a353a687076da640af0
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-09-28 23:19:19 +0100

    If statement

---
+++ JavaLearning/ConditionalStatements.java
@@ -0,0 +1,41 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+    
+        // If statemnet
+        int age = 18;
+        if (age == 18){
+            System.out.println("User is 18");
+        } else if (age >= 18){
+            System.out.println("User is over 18");
+        } else {
+            System.out.println("User is under 18");
+        }
+        
+        // Number checking
+        int[] num = {8,5}; 
+        
+        if (num[0] == num[1]){
+            System.out.println("Numbers are equal");
+        } else if (num[0] > num[1]){
+            System.out.println("First number is big");
+        } else {
+            System.out.println("Second number is big");
+        }	
+    }
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
