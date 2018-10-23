e76bebdcb00b2b34cb30f735b7a61e566fec7534
Radhika Sivarajan <radhikaraj333@gmail.com>
2018-10-23 23:39:30 +0100

    Comments

--- CurrencyConverter/app/src/main/java/com/radhikasivarajan/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/radhikasivarajan/currencyconverter/MainActivity.java
@@ -11,14 +11,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void convertFunction(View view){
 
-//        Get the amount, convert to double, multiply with current convertion rate and display
+        //Get the amount, convert to double, multiply with current convertion rate and display
         EditText getTheAmount = (EditText) findViewById(R.id.getAmount);
         Double dollarAmountDouble = Double.parseDouble(getTheAmount.getText().toString());
         Double poundAmount = dollarAmountDouble * 0.76;
 
-//        Toast.makeText(this, "£" + poundAmount.toString(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "£" + poundAmount.toString(), Toast.LENGTH_SHORT).show();
 
-//        With 2 decimal palces
+        //With 2 decimal places
         Toast.makeText(this, "£" + String.format("%.2f",poundAmount), Toast.LENGTH_SHORT).show();
 
         Log.i("Test","It works");
